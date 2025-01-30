5e19b8477180640d5224e03984c50333aa580d9b
Shevchenko Andrey <demorest49de@gmail.com>
2025-01-30 12:18:01 +0300

    Changes OutlineBell.tsx package.json

--- src/assets/components/OutlineBell.tsx
+++ src/assets/components/OutlineBell.tsx
@@ -1,15 +1,14 @@
 'use client'
 
-import { memo, Ref } from 'react'
+import { memo } from 'react'
 
-const SvgOutlineBell = ({ ...props }, ref: Ref<never>) => (
+const SvgOutlineBell = ({ ...props }, ) => (
   <svg
     fill={'none'}
     height={'1em'}
     viewBox={'0 0 24 24'}
     width={'1em'}
     xmlns={'http://www.w3.org/2000/svg'}
-    ref={ref}
     {...props}
   >
     <path
